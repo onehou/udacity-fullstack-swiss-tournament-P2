0e168672ba546f3052b6e41abc4a738f0e65e258
p00gz <bloopsocket@gmail.com>
2015-07-02 17:33:10 -0500

    testing global

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -10,4 +10,3 @@ su postgres -c 'createuser -dRS vagrant'
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
-
